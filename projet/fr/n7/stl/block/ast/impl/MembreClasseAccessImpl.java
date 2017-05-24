9aaea0d67e577f6ef73e5ffbb9be5d587b653a60
duncan <duncan.deveaux@gmail.com>
2017-05-24 03:38:27 +0200

    Accès aux membres de classe / arguments... déclarés.

--- projet/fr/n7/stl/block/ast/impl/MembreClasseAccessImpl.java
+++ projet/fr/n7/stl/block/ast/impl/MembreClasseAccessImpl.java
@@ -28,6 +28,7 @@ import fr.n7.stl.tam.ast.TAMFactory;
 public class MembreClasseAccessImpl implements Expression {
     
     protected String PREFIX = "";
+    
     protected boolean verified = false;
     
     public enum Identifier {THIS, SUPER, ARGUMENT, UNKNOWN};
@@ -51,6 +52,10 @@ public class MembreClasseAccessImpl implements Expression {
         this.membreAccede = new AppelOuAcces();
     }
     
+    public void setVerified(boolean _verified) {
+        this.verified = _verified;
+    }
+    
     public void setNomAcces(String _nom) {
         this.membreAccede.setNom(_nom);
     }
@@ -61,6 +66,14 @@ public class MembreClasseAccessImpl implements Expression {
     
     public void setArgumentsAcces(LinkedList<Expression> _args) {
         this.membreAccede.setArguments(_args);
+    }
+    
+    public String getNomAcces() {
+        return this.membreAccede.getNom();
+    }
+    
+    public LinkedList<Expression> getArgumentsAcces() {
+        return this.membreAccede.getArguments();
     }
     
     /* (non-Javadoc)
@@ -96,20 +109,88 @@ public class MembreClasseAccessImpl implements Expression {
     private String identifierToString(Identifier id) {
         if (id == Identifier.THIS) return "this.";
         else if (id == Identifier.SUPER) return "super.";
-        else if (id == Identifier.ARGUMENT) return "[arg] ";
+        else if (id == Identifier.ARGUMENT) return "[arg]";
         else return "";
+    }
+    
+    /** Declare return Type and method access arguments. */
+    public void declare(List<InterfaceDeclaration> interfaces, List<ClasseDeclaration> classes, Classe classeMere, MethodImpl methodeMere, Block blocPere) throws ToDeclaredException {
+        
+        //Déclaration des arguments.
+        if (membreAccede.getArguments() != null) {
+            
+            LinkedList<Expression> nouv_expr = new LinkedList<Expression>();
+            for (Expression expr : membreAccede.getArguments()) {
+                nouv_expr.add( expr.toDeclared( interfaces, classes, classeMere, methodeMere, blocPere ) );
+            }
+        
+            membreAccede.setArguments(nouv_expr);
+        }
+        
+        //Déclaration du type.
+        //if (type != null) {
+        //   this.type = type.toDeclared(interfaces, classes, classeMere);
+        //}
     }
     
     /* (non-Javadoc)
 	 * @see fr.n7.stl.block.ast.Expression#toDeclared()
 	 */
 	@Override
-	public Expression toDeclared(List<InterfaceDeclaration> interfaces, List<ClasseDeclaration> classes, Classe classeMere, MethodImpl methodeMere, Block blocPere) {
-		//We will now try to verify the access.
-        PREFIX = (methodeMere != null) ? methodeMere.getNom() : "[methodeMere nulle]";
+	public Expression toDeclared(List<InterfaceDeclaration> interfaces, List<ClasseDeclaration> classes, Classe classeMere, MethodImpl methodeMere, Block blocPere) throws ToDeclaredException {
+		
+        MembreClasseAccessImpl declared;
         
-        return this;
+        if (this.base != null) {
+            
+            Identifier _base = this.base;
+            String rootName = membreAccede.getNom();
+        
+            if (_base == Identifier.UNKNOWN) {
+                //Can be a THIS (or a super ?) or a Parameter access.
+                //As the prefix this. was not given, parameters are prioritized.
+                if (knownInMethod(methodeMere, rootName)) {
+                    _base = Identifier.ARGUMENT;
+                } else if (knownInClass(classeMere, rootName)) { //Trying to find it as a class member or method.
+                    _base = Identifier.THIS;
+                } else {
+                    throw new ToDeclaredException("\""+rootName+"\" n'est ni un paramètre de la méthode " + methodeMere.getNom() +
+                                                    " ni un membre de la classe " + classeMere.getNom() + ".");
+                }
+            } else if (base == Identifier.THIS && !knownInClass(classeMere, rootName)) {
+                throw new ToDeclaredException("\""+rootName+"\" n'est pas un membre de la classe " + classeMere.getNom() + ".");
+            }
+            
+            declared = new MembreClasseAccessImpl(_base);
+        } else {
+            declared = new MembreClasseAccessImpl((MembreClasseAccessImpl) this.access.toDeclared(interfaces, classes, classeMere, methodeMere, blocPere));
+        }
+        
+        
+        declared.setNomAcces(this.getNomAcces());
+        declared.setArgumentsAcces(this.getArgumentsAcces());
+        
+        declared.declare(interfaces, classes, classeMere, methodeMere, blocPere);
+        declared.setVerified(true);
+        
+        return declared;
 	}
+    
+    /** Renvoie true si nom est un nom d'attribut ou de méthode de la classe. */
+    private boolean knownInClass(Classe classe, String nom) {
+        return (classe.getAttribut(nom).isPresent() || classe.getMethode(nom).isPresent());
+    }
+    
+    /** Renvoie true si nom est un nom d'argument de la méthode. */
+    private boolean knownInMethod(MethodImpl methode, String nom) {
+        LinkedList<Argument> _args = methode.getArguments();
+        for (Argument _arg : _args) {
+            if (_arg.getName().equals(nom))
+                return true;
+        }
+        
+        return false;
+    }
     
     /* (non-Javadoc)
      * @see fr.n7.stl.block.ast.Expression#getType()
